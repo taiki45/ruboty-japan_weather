8accd1466956f2724354cf0e5d60808ce1efdb12
Taiki ONO <taiks.4559@gmail.com>
2014-04-30 21:47:57 +0900

    Fix regexp to match better in ellen's convention

--- lib/ellen/handlers/japan_weather.rb
+++ lib/ellen/handlers/japan_weather.rb
@@ -1,7 +1,7 @@
 module Ellen
   module Handlers
     class JapanWeather < Base
-      on /の天気/, name: "japan_weather", description: "Reply weather forecast"
+      on /.*の天気/, name: "japan_weather", description: "Reply weather forecast"
 
       def japan_weather(message)
         Ellen::Actions::JapanWeather.new(message).call
