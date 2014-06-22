b510a93cb9059fef350d395582487d9a0c6b2437
Taiki ONO <taiki-ono@cookpad.com>
2014-06-22 22:14:11 +0900

    Add command type interface

--- lib/ruboty/actions/japan_weather.rb
+++ lib/ruboty/actions/japan_weather.rb
@@ -8,13 +8,46 @@ module Ruboty
       private
 
       def forecast
-        look_for(message.body).to_s
+        if captures = cmd?(message.match_data.captures)
+          look_by_cmd(*captures).to_s
+        elsif body = jp?(message.match_data.captures)
+          look_by_jp(body).to_s
+        else
+          message.reply('Unmatched...')
+        end
       end
 
-      def look_for(body)
+      def look_by_cmd(place, day)
+        WeatherJp.get(place, convert(day))
+      rescue WeatherJp::WeatherJpError => e
+        "Error: #{e}"
+      end
+
+      def look_by_jp(body)
         WeatherJp.parse(body)
       rescue WeatherJp::WeatherJpError => e
         "Error: #{e}"
+      end
+
+      def cmd?(captures)
+        captures[1..2] if captures[1] && captures[2]
+      end
+
+      def jp?(captures)
+        captures.first if captures.first
+      end
+
+      def convert(day)
+        case day
+        when /今日|きょう|今|いま/u
+          :today
+        when /明日|あした/u
+          :tomorrow
+        when /明後日|あさって/u
+          :day_after_tomorrow
+        else
+          day
+        end
       end
     end
   end
--- lib/ruboty/handlers/japan_weather.rb
+++ lib/ruboty/handlers/japan_weather.rb
@@ -1,7 +1,11 @@
 module Ruboty
   module Handlers
     class JapanWeather < Base
-      on /.*の天気/, name: "japan_weather", description: "Reply weather forecast"
+      on(
+        /((?<jp>.*の天気.*)|tenki\s+(?<place>.*)\s+(?<day>.*))/,
+        name: "japan_weather",
+        description: "Reply weather forecast"
+      )
 
       def japan_weather(message)
         Ruboty::Actions::JapanWeather.new(message).call
